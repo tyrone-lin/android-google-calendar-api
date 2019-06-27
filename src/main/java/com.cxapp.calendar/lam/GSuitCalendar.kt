49861a62c42f48e97ab49742dfa85aa35de2e72a
Brolin <flyblue.lin@thecxapp.com>
2019-06-27 10:25:51 +0800

    G Suit授权

--- src/main/java/com.cxapp.calendar/lam/GSuitCalendar.kt
+++ src/main/java/com.cxapp.calendar/lam/GSuitCalendar.kt
@@ -1,8 +1,10 @@
 package com.cxapp.calendar.lam
 
+import android.Manifest
 import android.accounts.AccountManager
 import android.app.Activity
 import android.content.Context
+import androidx.fragment.app.FragmentActivity
 import com.google.api.client.extensions.android.http.AndroidHttp
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import com.google.api.client.json.jackson2.JacksonFactory
@@ -11,14 +13,26 @@ import com.google.api.services.calendar.CalendarScopes
 import com.google.api.services.calendar.model.CalendarList
 import com.infrastructure.common.base.BasicActivity
 import com.infrastructure.common.logger.printInfo
+import com.infrastructure.common.permissions.PermissionsBuilder
 import com.infrastructure.filebase.kv.KV
 import com.infrastructure.filebase.kv.insertOrUpdate2DB
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.*
 
 /**
  * @author: Brolin
  * @create: 2019/06/25 17:12
  */
-class GSuitCalendar(private val mContext: Context) {
+class GSuitCalendar(private val mContext: FragmentActivity) {
+
+    init {
+        PermissionsBuilder.instance(mContext){
+            it.permissions(Manifest.permission.GET_ACCOUNTS, Manifest.permission.GET_ACCOUNTS)
+                .access()
+        }
+    }
 
     private companion object {
         private val GSUIT_CALENDAR_CHOSEN_ACCOUNT = "GSUIT_CALENDAR_CHOSEN_ACCOUNT"
@@ -28,7 +42,7 @@ class GSuitCalendar(private val mContext: Context) {
 
     private val mCredential by lazy {
         JacksonFactory.getDefaultInstance()
-        val credential = GoogleAccountCredential.usingOAuth2(mContext, setOf(CalendarScopes.CALENDAR))
+        val credential = GoogleAccountCredential.usingOAuth2(mContext, Collections.singleton(CalendarScopes.CALENDAR))
         credential.selectedAccountName = chosenAccount
         return@lazy credential
     }
@@ -36,7 +50,7 @@ class GSuitCalendar(private val mContext: Context) {
     private val mClient by lazy {
         val transport = AndroidHttp.newCompatibleTransport()
         val jsonFactory = JacksonFactory.getDefaultInstance()
-        return@lazy Calendar.Builder(transport, jsonFactory, mCredential)
+        return@lazy com.google.api.services.calendar.Calendar.Builder(transport, jsonFactory, mCredential)
             .setApplicationName("Google-CalendarAndroidSample/1.0")
             .build()
     }
@@ -67,16 +81,18 @@ class GSuitCalendar(private val mContext: Context) {
                 if (accountName != null) {
                     mCredential.selectedAccountName = accountName
                     chosenAccount = accountName
+                    getCalendarList()
                 }
             }
         }
     }
 
 
-    fun getCalendarList(): CalendarList {
-        val feed = mClient.calendarList().list().execute()
-        feed.printInfo()
-        return feed
+    fun getCalendarList() {
+        Single.create<Boolean> {
+            val feed = mClient.calendarList().list().execute()
+            feed.printInfo()
+        }.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe()
     }
 
     fun getEvents(){
